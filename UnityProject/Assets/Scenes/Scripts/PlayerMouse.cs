f5c13c48a2dd8c96a642a4cb5d7404e12143d458
Sam LaGrave <srl0011@uah.edu>
2019-11-08 19:33:18 -0600

    Movement is there

---
+++ UnityProject/Assets/Scenes/Scripts/PlayerMouse.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouse : MonoBehaviour
+{
+    // Control Variables
+    [SerializeField]
+    public float sensitivity = 5.0f;
+    [SerializeField]
+    public float smoothing = 2.0f;
+    public GameObject character;
+    private Vector2 mouseLook;
+    private Vector2 smoothV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // The maths
+        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, mouseDelta.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, mouseDelta.y, 1f / smoothing);
+        mouseLook += smoothV;
+
+        // Move the camera and character
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+}
---
+++ UnityProject/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Variables to be used
+    public float speed = 10.0f;
+    public float forward;
+    public float strafe;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Turn off cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movement
+        forward = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        strafe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(strafe, 0, forward);
+        
+        if (Input.GetKeyDown("escape")) {
+            // turn on the cursor
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}

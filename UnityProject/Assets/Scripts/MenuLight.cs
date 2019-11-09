bac18cd7d5e32a44a3d28074f791ecd69b3297a5
Sam LaGrave <srl0011@uah.edu>
2019-11-09 13:47:53 -0600

    Menu stuff

---
+++ UnityProject/Assets/Scripts/MenuLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuLight : MonoBehaviour
+{
+    private bool on = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var q = HSRandom.Next(10);
+        if(q==0)
+        {
+            on = !on;
+        }
+
+        if(on)
+        {
+            GetComponent<Light>().intensity = 7;
+        }
+        else
+        {
+            GetComponent<Light>().intensity = 0;
+        }
+    }
+}

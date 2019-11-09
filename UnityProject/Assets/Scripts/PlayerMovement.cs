b350eb7edb21e860ad9e39a938fcccd7017052fe
IAmAPersson <iamaperson620@gmail.com>
2019-11-08 20:27:18 -0600

    you can jump now lmao

--- UnityProject/Assets/Scripts/PlayerMovement.cs
+++ UnityProject/Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,19 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 10.0f;
     public float forward;
     public float strafe;
+    private float vvel = 0f;
+    private bool iscol = false;
 
+    void OnCollisionEnter(Collision collision)
+    {
+        //if hitting the floor
+        if (collision.gameObject.name == "Plane")
+        {
+            //do not lmao
+            iscol = true;
+            vvel = 0f;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +32,28 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         // Movement
         forward = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         strafe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         transform.Translate(strafe, 0, forward);
-        
-        if (Input.GetKeyDown("escape")) {
+
+        if (Input.GetKeyDown("escape"))
+        {
             // turn on the cursor
             Cursor.lockState = CursorLockMode.None;
+        }
+        else if (Input.GetKeyDown("space") && vvel == 0)
+        {
+            vvel = 0.2f;
+            iscol = false;
+        }
+
+        if (!iscol)
+        {
+            //decrease vertical velocity
+            vvel -= 0.0075f;
+            transform.Translate(0, vvel, 0);
         }
     }
 }
